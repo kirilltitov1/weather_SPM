590be3e44e2d9cbca60e823c7b47453c94976720
Kirill <kirill.titov@outlook.com>
2020-12-01 19:11:32 +0300

    remove alpha

--- weather_swiftManager/App/BusinessLogic/Services/SearchCityWeatherReport/Package.swift
+++ weather_swiftManager/App/BusinessLogic/Services/SearchCityWeatherReport/Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SearchCityWeatherReport",
-            dependencies: ["Alamofire", "RxSwift"]),
+            dependencies: ["Alamofire", "RxSwift", "ErrorHandler"]),
         .testTarget(
             name: "SearchCityWeatherReportTests",
             dependencies: ["SearchCityWeatherReport"]),
